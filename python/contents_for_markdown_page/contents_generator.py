cb4854140dc264451904e356b84d3ebbcea28609
Andriy Huliy <andriy.huliy@gmail.com>
2019-01-10 22:59:01 +0200

    The next working draft

--- python/contents_for_markdown_page/contents_generator.py
+++ python/contents_for_markdown_page/contents_generator.py
@@ -3,37 +3,38 @@ import re
 MAX_HEADER_LEVEL = 6
 INDENT_SPACES_NUM = 7
 
+
 def find_headings(file_path):
 
-    with open (file_path, "r") as in_file:
+    with open(file_path, "r") as in_file:
+        heading_level_lst = []
         for line in in_file:
-            level = heading_level(line)
-            if level is not None:
-                heading = extract_heading(line)
-                print(heading)
-                print(heading_to_md_link(heading))
+            heading_level = extract_heading(line)
+            if heading_level:
+                heading_level_lst.append(heading_level)
+
+    return heading_level_lst
 
 
-def heading_level(text_line):
+def extract_heading(text_line):
+    """
 
+    :rtype: tuple
+    """
     match_pattern = "\s{{0,{}}}(#{{1,{}}})\s.*".format(
                                       INDENT_SPACES_NUM,
                                       MAX_HEADER_LEVEL)
 
-    match_result = re.match(match_pattern,text_line)
+    match_result = re.match(match_pattern, text_line)
 
-    if match_result is not None:
-        level = len(match_result.group(1))
-    else:
-        level = None
-
-    return level
-
-def extract_heading(text_line):
-
+    if match_result is None:
+        return ()
+        
+    level = len(match_result.group(1))
     heading = text_line.strip()
     heading = heading.strip(" #")
-    return heading
+    
+    return (heading, level)
 
 
 def heading_to_md_link(header):
@@ -41,3 +42,22 @@ def heading_to_md_link(header):
     md_header = header.lower().replace(" ", "-")
 
     return "[{}](#{})".format(header, md_header)
+
+
+if __name__ == '__main__':
+
+    file_path = "test_page.md"
+    headings_and_levels = find_headings(file_path)
+    if headings_and_levels:
+        base_level = headings_and_levels[0][1]
+
+        for index in range(len(headings_and_levels)):
+            heading, level = headings_and_levels[index]
+            if index != 0:
+                pass
+            subheading_indent = (level-base_level) * 2 * " "
+            contents_item = subheading_indent +  "- " + heading_to_md_link(heading)
+            print(contents_item)
+
+    else:
+        print("Headings have not been found")
